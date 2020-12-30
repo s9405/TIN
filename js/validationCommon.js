3d04a5cecac3e3cd9dc59e7b905dfb05b51c9d54
s9405 <s9405@pjwstk.edu.pl>
2020-12-30 15:25:55 +0100

    validation v2

--- js/validationCommon.js
+++ js/validationCommon.js
@@ -1,8 +1,8 @@
 function resetErrors(inputs, errorTexts, errorInfo) {
-    for(let i=0; i<inputs.length; i++) {
+    for (let i = 0; i < inputs.length; i++) {
         inputs[i].classList.remove("error_input");
     }
-    for(let i=0; i<errorTexts.length; i++) {
+    for (let i = 0; i < errorTexts.length; i++) {
         errorTexts[i].innerText = "";
     }
     errorInfo.innerText = "";
--- js/validationPlayerForm.js
+++ js/validationPlayerForm.js
@@ -1,37 +1,37 @@
 
 
-function validateForm(){
+function validateForm() {
     let valid = true;
     const firstNameInput = document.getElementById('firstName');
-const lastNameInput = document.getElementById('lastName');
-const emailInput = document.getElementById('email');
+    const lastNameInput = document.getElementById('lastName');
+    const emailInput = document.getElementById('email');
 
-const errorFirstName = document.getElementById('errorFirstName');
-const errorLastName = document.getElementById('errorLastName');
-const errorEmail = document.getElementById('errorEmail');
-const errorsSummary = document.getElementById('errorsSummary');
+    const errorFirstName = document.getElementById('errorFirstName');
+    const errorLastName = document.getElementById('errorLastName');
+    const errorEmail = document.getElementById('errorEmail');
+    const errorsSummary = document.getElementById('errorsSummary');
 
-resetErrors([firstNameInput, lastNameInput, emailInput], [errorFirstName, errorLastName, errorEmail], errorsSummary);
+    resetErrors([firstNameInput, lastNameInput, emailInput], [errorFirstName, errorLastName, errorEmail], errorsSummary);
 
 
     if (!checkRequired(firstNameInput.value)) {
         valid = false;
         firstNameInput.classList.add("error-input");
         errorFirstName.innerText = "Pole jest wymagane";
-    } else if (!checkTextLengthRange(firstNameInput.value, 2, 60)) {
+    } else if (!checkTextLengthRange(firstNameInput.value, 2, 20)) {
         valid = false;
         firstNameInput.classList.add("error-input");
-        errorFirstName.innerText = "Pole powinno zawierać od 2 do 60 znaków";
+        errorFirstName.innerText = "Pole powinno zawierać od 2 do 20 znaków";
     }
 
     if (!checkRequired(lastNameInput.value)) {
         valid = false;
         lastNameInput.classList.add("error-input");
         errorLastName.innerText = "Pole jest wymagane";
-    } else if (!checkTextLengthRange(lastNameInput.value, 2, 60)) {
+    } else if (!checkTextLengthRange(lastNameInput.value, 2, 20)) {
         valid = false;
         lastNameInput.classList.add("error-input");
-        errorLastName.innerText = "Pole powinno zawierać od 2 do 60 znaków";
+        errorLastName.innerText = "Pole powinno zawierać od 2 do 20 znaków";
     }
 
     if (!checkRequired(emailInput.value)) {
